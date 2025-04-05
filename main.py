8b72e4e3ae30dea55cce52c069935fdf68faea4c
Roger <mattle.roger@bluewin.ch>
2025-04-05 16:57:27 +0200

    changed start process listener

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from contextlib import asynccontextmanager
 import uvicorn
 
 import threading
@@ -9,26 +10,26 @@ import time
 
 import engine_data_reader
 
+#----------------------------------------------------------------------------------------
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("starting up engine data reader :) ")
+    engine_data_thread = threading.Thread(target=engine_data_reader.read_engine_data, daemon=True)
+    engine_data_thread.start()
+    yield
+    
+    print("shutting down :( ")
 
-app = FastAPI()
-
-
-# mount the static directory
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-templates = Jinja2Templates(directory="templates")
-
-
+    
+app = FastAPI(lifespan=lifespan)    # create fastAPI app with lifespan parameter for proper behav. on startup
+app.mount("/static", StaticFiles(directory="static"), name="static")    # mount the static directory
+templates = Jinja2Templates(directory="templates") 
+#----------------------------------------------------------------------------------------   
+    
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
     # render and return the index.html template
     return templates.TemplateResponse("index.html",{"request": request})
-
-
-@app.on_event("startup")
-def startup_event():
-    engine_data_thread = threading.Thread(target=engine_data_reader.read_engine_data, daemon=True)
-    engine_data_thread.start()
     
 
 @app.get("/favicon.ico")
@@ -39,6 +40,9 @@ async def favicon():
 @app.get("/api/engine-data")
 def get_engine_data():
     return engine_data_reader.engine_data
+
+
+#----------------------------------------------------------------------------------------
 
 
 
